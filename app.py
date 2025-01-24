df72b10b300bbb99834d64a9f06146428a267083
Rajat Baidya <rajat.baidya@nepal.flextecs>
2025-01-24 16:10:36 +0545

    added new tests

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 print("hello world")
+<<<<<<< HEAD
 print("lelo khalo")
+=======
+print("lelo khalo")
+>>>>>>> 0d929cc (added new tests)
---
+++ tests/test_new_features.py
@@ -0,0 +1,11 @@
+import pytest
+def add_number(a,b):
+    return a+b
+
+def test_add_numbers():
+    assert add_number(2,3) == 5
+    assert add_number(3,3) == 6
+    assert add_number(2,-2) == 0
+
+
+    
