22d7275a3b599bbd09f8d1debf2dee4b7800d051
Raz-at <razbaidya1@gmail.com>
2025-01-27 11:00:33 +0545

    comment the unittest

--- unittest.py
+++ unittest.py
@@ -1,14 +1,14 @@
-import unittest
-from app import add, subtract
+# import unittest
+# from app import add, subtract
 
-class TestApp(unittest.TestCase):
+# class TestApp(unittest.TestCase):
     
-    def test_add(self):
-        self.assertEqual(add(1, 2), 3)
+#     def test_add(self):
+#         self.assertEqual(add(1, 2), 3)
     
-    def test_subtract(self):
-        self.assertEqual(subtract(2, 1), 1)
+#     def test_subtract(self):
+#         self.assertEqual(subtract(2, 1), 1)
         
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
